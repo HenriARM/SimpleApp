b59d1b4edbec50b54c8412a299bc38e0b74d535d
HenriARM <henriarm@yandex.ru>
2019-06-15 14:04:06 +0300

    Added Custom Validator for name property with RegExp

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import {FormsModule} from "@angular/forms";
+import { ForbiddenValidatorDirective } from './forbidden-validator.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    ForbiddenValidatorDirective
   ],
   imports: [
     BrowserModule,
