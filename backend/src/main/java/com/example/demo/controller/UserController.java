a3cc3afe5863e166885aec6c149969b5b07e4726
HenriARM <henriarm@yandex.ru>
2019-07-14 01:31:05 +0300

    Added User Entity and Repository

---
+++ backend/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import com.example.demo.persistence.entity.User;
+import com.example.demo.persistence.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(path = "")
+public class UserController {
+    private final UserRepository userRepository;
+
+    @Autowired // bean will be assigned to variable
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping(path = "/add")
+    public @ResponseBody String addNewUser(@RequestParam String name, @RequestParam String email) {
+        User user = new User();
+        user.setName(name);
+        user.setEmail(email);
+        userRepository.save(user);
+        return "Saved";
+    }
+
+    @GetMapping(path = "/all")
+    public @ResponseBody Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+}
